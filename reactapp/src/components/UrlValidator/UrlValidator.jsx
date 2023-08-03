727acf0ee02c7bc648534226c2993dca949cbc9f
Iamneo <techadmin@iamneo.ai>
2023-08-03 05:44:05 +0000

    Backup Commit

--- reactapp/src/components/UrlValidator/UrlValidator.jsx
+++ reactapp/src/components/UrlValidator/UrlValidator.jsx
@@ -1,26 +1,26 @@
 import React, { useEffect, useState } from 'react';
  
-function UrlValidator() {
-
-    let [disabled, setDisabled] = useState(false);
-
+const UrlValidator = () => {
+ 
+    let [disabled,setDisabled] = useState(false);
+ 
     const isValidateDomain = (d) => {
-        if (d.startsWith("www.") && d.endsWith(".com")) {
+        if(d.startsWith("www.") && d.endsWith(".com")) {
             return true;
         }
         return false;
-    };
-
+    }
+ 
     const convertToPath = (input) => {
         return '/' + input.split(" ").join("/");
-    };
-
+    }
+ 
     const convertJson = (jsonString) => {
         const jsonObject = JSON.parse(jsonString);
         const params = "?" + Object.entries(jsonObject).map((e) => e.join("=")).join("&");
         return params;
-    };
-
+    }
+ 
     const isValidJson = (jsonString) => {
         try {
             JSON.parse(jsonString);
@@ -28,42 +28,39 @@ function UrlValidator() {
         } catch (e) {
             return false;
         }
-    };
-
-    const validateForm = (domain, path, method, body) => {
+    }
+ 
+    const validateForm = (domain,path,method,body) => {
         if (!isValidateDomain(domain)) {
             console.log("went");
             return "Invalid URL! Please recheck your URL";
         }
         if (method === "GET") {
-            if (Object.keys(body).length === 0)
-                return "";
-            if (!isValidJson(body))
-                return "Error in the Body of the Query Params";
+            if (Object.keys(body).length === 0 ) return "";
+            if (!isValidJson(body)) return "Error in the Body of the Query Params";
         }
         if (method === "POST" || method === "PUT") {
-            if (Object.keys(body).length === 0)
+            if(Object.keys(body).length === 0) 
                 return "Error in the Body";
-            if (!isValidJson(body)) {
+            if(!isValidJson(body)) {
                 return "Error in the Body";
             }
         }
         return "";
-    };
-
+    }
+ 
     const handleSubmit = async (e) => {
         e.preventDefault();
         let domain = e.target[0].value;
         let path = e.target[1].value;
         let method = e.target[2].value;
         let body = e.target[3].value;
-        let tmp_msg = validateForm(domain, path, method, body);
-        console.log(tmp_msg);
-        if (tmp_msg === "") {
+        let tmp_msg = validateForm(domain,path,method,body);
+        console.log(tmp_msg)
+        if(tmp_msg==="") {
             console.log("success");
-            let link = domain + convertToPath(path);
-            if (Object.keys(body).length != 0 && method === "GET")
-                link = link + convertJson(body);
+            let link = domain + convertToPath(path)
+            if (Object.keys(body).length != 0 && method === "GET") link = link + convertJson(body);
             console.log(typeof link);
             document.getElementById("message").textContent = link.trim();
             console.log(link.trim());
@@ -71,13 +68,13 @@ function UrlValidator() {
         else {
             document.getElementById("message").textContent = tmp_msg;
         }
-
-    };
+ 
+    }
     return (
         <div className="main-div">
             <div data-testid="message" id="message"></div>
             <form data-testid="submit" onSubmit={handleSubmit}>
-
+ 
                 <label>
                     Domain:
                     <input data-testid="domain" type="text" />
@@ -90,10 +87,10 @@ function UrlValidator() {
                 <br />
                 <label>
                     Method:
-                    <select data-testid="method" defaultValue="GET" id="method" onChange={(e) => {
+                    <select data-testid="method" defaultValue="GET" id="method" onChange={(e)=>{
                         console.log(e);
-                        (document.getElementById("method").value === "DELETE") ? setDisabled(true) : setDisabled(false);
-                    } }>
+                        (document.getElementById("method").value==="DELETE") ? setDisabled(true) : setDisabled(false);
+                    }}>
                         <option value="GET">GET</option>
                         <option value="POST">POST</option>
                         <option value="PUT">PUT</option>
@@ -103,8 +100,9 @@ function UrlValidator() {
                 <br />
                 <label>
                     Body:
-                    <textarea data-testid="body"
-                        disabled={disabled} />
+                    <textarea data-testid="body" 
+                        disabled={disabled}  
+                     />
                 </label>
                 <br />
                 <button type="submit">Validate</button>
